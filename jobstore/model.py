9fb7726f07d48695c70a49cfdc8d43f6478e1978
Jessica Nash <janash@vt.edu>
2019-10-08 09:46:52 -0400

    update model to have user and user projects table

--- jobstore/model.py
+++ jobstore/model.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -13,13 +14,17 @@ class Flowchart(Base):
     __tablename__ = 'flowcharts'
 
     id = Column(String, nullable=False, primary_key=True, unique=True)
-    description = Column(String, nullable=True)
-    flowchart_file = Column(String, nullable=False)
+    description = Column(String(1000), nullable=True)
+    flowchart_file = Column(String(5000), nullable=False)
 
     def __init__(self, **kwargs):
         self.id = kwargs['id']
         self.description = kwargs['description']
         self.flowchart_file = kwargs['flowchart_file']
+    
+    def __repr__(self):
+        return F"Flowchart(id={self.id}, description={self.description}, flowchart_file={self.flowchart_file}')"
+
 
 class Job(Base):
     __tablename__ = 'jobs'
@@ -27,11 +32,20 @@ class Job(Base):
     path = Column(String, nullable=False, primary_key=True)
     flowchart_id = Column(String, ForeignKey("flowcharts.id"))
     submission_date = Column(DateTime, nullable=False, default=datetime.utcnow)
+    author = Column(String, nullable=True)
 
     def __init__(self, **kwargs):
         self.path = kwargs['path']
         self.flowchart_id  = kwargs['flowchart_id']
         self.submission_date = kwargs['submission_date']
+        try:
+            self.author = kwargs['author']
+        except KeyError:
+            # Allow the author to not be specified.
+            pass
+    
+    def __repr__(self):
+        return F"Job(path={self.path}, flowchart_id={self.flowchart_id}, submission_date={self.submission_date})"
 
 class Project(Base):
     __tablename__ = "projects"
@@ -44,3 +58,17 @@ class JobProject(Base):
 
     job_path = Column(String, ForeignKey('jobs.path'), primary_key=True)
     project = Column(String, ForeignKey('projects.name', primary_key=True))
+
+class User(Base):
+    __tablename__ = "users"
+    username = Column(String(20), unique=True, nullable=False, primary_key=True)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(60), nullable=False)
+
+class UserProject(Base):
+    __tablename__ = "user_projects"
+
+    user = Column(String, ForeignKey('users.username'), primary_key=True)
+    project = Column(String, ForeignKey('projects.name'), primary_key=True)
+
+    
